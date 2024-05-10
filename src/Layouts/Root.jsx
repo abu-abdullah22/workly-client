9484e981f170e3e5f0acc4fcf1f017fa29da1722
abu-abdullah22 <rorschachmayhem@gmail.com>
2024-05-10 21:53:53 +0600

    firebase setup primary done

---
+++ src/Layouts/Root.jsx
@@ -0,0 +1,15 @@
+const Root = () => {
+    return (
+        <div>
+            <h1>HELLOOOOOO</h1>
+            {/* Navbar */}
+
+            {/* Outlet */}
+
+            {/* Footer */}
+            
+        </div>
+    );
+};
+
+export default Root;
