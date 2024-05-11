f007b4d0940858cfb0e019077a037699866c29ac
abu-abdullah22 <rorschachmayhem@gmail.com>
2024-05-11 16:29:36 +0600

    initialized tab section

--- src/Components/TabSection.jsx
+++ src/Components/TabSection.jsx
@@ -1,10 +1,12 @@
+/* eslint-disable react/prop-types */
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
+import JobCard from './JobCard';
 
-const TabSection = () => {
+const TabSection = ({ jobs }) => {
     return (
         <Tabs>
-            <h1 className='text-center text-3xl font-bold my-8'>Job by category</h1>
+            <h1 className='text-center text-3xl font-bold my-8'>Jobs by category</h1>
             <div className='container mx-auto'>
                 <div className='flex items-center justify-center'>
                     <TabList>
@@ -17,19 +19,46 @@ const TabSection = () => {
                 </div>
 
                 <TabPanel>
-                    <h2>All</h2>
+                    <div className='grid grid-cols-1 gap-8 mt-8 xl:mt-14 md:grid-cols-2 xl:grid-cols-3'>
+                        {jobs.map(job =>
+                            <JobCard key={job._id}></JobCard>
+                        )}
+                    </div>
+                </TabPanel>
+
+
+                <TabPanel>
+                    <div className='grid grid-cols-1 gap-8 mt-8 xl:mt-14 md:grid-cols-2 xl:grid-cols-3'>
+                        {jobs.filter(j => j.job_category
+                            === 'on_site').map(job =>
+                                <JobCard key={job._id}></JobCard>
+                            )}
+                    </div>
+                </TabPanel>
+
+                <TabPanel>
+                    <div className='grid grid-cols-1 gap-8 mt-8 xl:mt-14 md:grid-cols-2 xl:grid-cols-3'>
+                        {jobs.filter(j => j.job_category
+                            === 'remote').map(job =>
+                                <JobCard key={job._id}></JobCard>
+                            )}
+                    </div>
                 </TabPanel>
                 <TabPanel>
-                    <h2>onsite</h2>
+                    <div className='grid grid-cols-1 gap-8 mt-8 xl:mt-14 md:grid-cols-2 xl:grid-cols-3'>
+                        {jobs.filter(j => j.job_category
+                            === 'hybrid').map(job =>
+                                <JobCard key={job._id}></JobCard>
+                            )}
+                    </div>
                 </TabPanel>
                 <TabPanel>
-                    <h2>remote</h2>
-                </TabPanel>
-                <TabPanel>
-                    <h2>hybrid</h2>
-                </TabPanel>
-                <TabPanel>
-                    <h2>part time</h2>
+                    <div className='grid grid-cols-1 gap-8 mt-8 xl:mt-14 md:grid-cols-2 xl:grid-cols-3'>
+                        {jobs.filter(j => j.job_category
+                            === 'part_time').map(job =>
+                                <JobCard key={job._id}></JobCard>
+                            )}
+                    </div>
                 </TabPanel>
             </div>
         </Tabs>
